01bb3fc00fce8780c5c295fbbffbbb3e32fef333
aokabi <aokabit@gmail.com>
2017-11-13 01:13:12 +0900

    other

--- kadai2/program1/target/debug/deps/program1-c029a6506a42ebee.d
+++
@@ -1,5 +0,0 @@
-C:\Users\aokabi\NumericCalculation\kadai2\program1\target\debug\deps\program1-c029a6506a42ebee.exe: src\main.rs
-
-C:\Users\aokabi\NumericCalculation\kadai2\program1\target\debug\deps\program1-c029a6506a42ebee.d: src\main.rs
-
-src\main.rs:
