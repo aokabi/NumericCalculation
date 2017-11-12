1f258181cd41e3a7f6359d0efb7ee71324693788
aokabi <aokabit@gmail.com>
2017-11-13 01:12:35 +0900

    vector plus vector by for loop

--- kadai2/program1/src/main.rs
+++ kadai2/program1/src/main.rs
@@ -2,15 +2,28 @@
 const N: usize = 3;
 
 fn main() {
-    let matrix_a: [[f64; N];N] = [[1., 0., 0.], [-3., 1., 0.], [2., -1., 1.]];
+    let matrix_a: [[f64; N]; N] = [[1., 0., 0.], [-3., 1., 0.], [2., -1., 1.]];
     let array_b: [f64; N] = [-5., 9., 4.];
     let mut array_y: [f64; N] = [0.; N];
-    println!("Hello, world!");
-    for i in 1..N {
+
+    for i in 0..N {
         array_y[i] = forward_substitute(i, &matrix_a[i], &array_b[i], &array_y);
-    } 
+    }
+
+    for y in &array_y {
+        println!("{}", y);
+    }
 }
 
-fn forward_substitute(i: usize, A: &[f64], b: &f64, y: &[f64]) -> f64{
-    b - A.iter().fold(0, |acc &x| acc + x) 
+fn forward_substitute(row: usize, a: &[f64], b: &f64, y: &[f64]) -> f64 {
+    //let iter: f64 = a.iter().zip(y.iter()).sum();
+    let mut sum = 0.;
+    if row == 0 {
+        return *b;
+    }
+
+    for j in 0..row {
+        sum += a[j] * y[j];
+    }
+    b - sum
 }
